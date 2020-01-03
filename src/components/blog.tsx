791b56911ed4fd28ced034fdbb5aaa3d59c102ce
trainto <trainto@gmail.com>
2020-01-03 18:44:25 +0900

    Fetch recent blog posts and show them

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import './blog.css';
 
-const BLOG_HOST = 'https://hisblog.yoursun.me';
+const BLOG_HOST = 'https://hisblog.yoursun.me/';
 
 interface IPost {
   title: string;
+  date: string;
   link: string;
 }
 
@@ -21,10 +23,28 @@ const Blog = () => {
           const match = page.match(/<body>.*<\/body>/is);
           if (match) {
             el.innerHTML = match[0];
+          } else {
+            return;
           }
 
           const postArr = Array.from(el.getElementsByClassName('blog-post'));
-          console.log(postArr);
+
+          const postsParsed: IPost[] = [];
+          postArr.forEach(post => {
+            const title = (post.getElementsByClassName('blog-post-title').item(0) as HTMLElement).innerText;
+            const date = (post.getElementsByClassName('blog-post-meta').item(0) as HTMLElement).innerText;
+            const link = post.getElementsByTagName('a')[0].href;
+
+            postsParsed.push({
+              title,
+              date: date.split('|')[0].trim(),
+              link: link.replace(/http:\/\/.*?\//i, BLOG_HOST)
+            });
+          });
+
+          if (postsParsed.length > 0) {
+            setPosts(postsParsed);
+          }
         }
       } catch (err) {
         // do Nothing
@@ -47,14 +67,11 @@ const Blog = () => {
 
         <ul className="mt-5">
           {posts.map((post, i) => (
-            <li key={i}>
+            <li className="mt-3" key={i}>
               <a href={post.link} target="_blank" rel="noopener noreferrer">
                 {post.title}
               </a>
-              <br />
-              <span className="ml-2 text-muted">
-                Journal of Korean Institute of Intelligent Systems, 2010, 2nd author
-              </span>
+              <small className="ml-2 text-muted">- {post.date}</small>
             </li>
           ))}
         </ul>
