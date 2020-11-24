af2c8f7c6f02f61e64d4b75ca18a248fb1da843e
trainto <trainto@gmail.com>
2020-11-24 16:06:31 +0900

    feat: add FE portfolio

--- src/components/portfolio/front-end.tsx
+++ src/components/portfolio/front-end.tsx
@@ -4,6 +4,25 @@ const FrontEnd = () => {
   return (
     <>
       <div className="row justify-content-center">
+        <div className="col-10 col-md-5 order-1 order-md-2 mt-3 text-center">
+          <img className="img-fluid rounded shadow" src={require('../../img/rbdialog.jpg')} alt="Air-Quality" />
+        </div>
+        <div className="col-12 col-md-6 mt-3 order-2 order-md-1 text-left text-center align-self-center">
+          <div className="highlite-color font-bigger-1">RB Dialog</div>
+          <small>
+            RB Dialog is an advanced development suite for creating conversational AI chatbots. It includes a visual
+            bot-building platform, collaboration and versioning tools.
+          </small>
+          <br />
+          <small>Jan.2020 - Dec.2020</small>
+          <br />
+          <br />
+          <div className="font-weight-bold">Skills:</div>
+          Typescript, Next.js(React)
+        </div>
+      </div>
+
+      <div className="row justify-content-center mt-5">
         <div className="col-10 col-md-5 mt-3 text-center">
           <img className="img-fluid rounded shadow" src={require('../../img/capstec.jpg')} alt="Capstec" />
         </div>
