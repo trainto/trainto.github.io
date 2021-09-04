33946b9d02fc2b249f2be74e7e57029fe9a3559e
Hakjoon Sim <hakjoon.sim@sk.com>
2021-09-04 13:25:45 +0900

    fix: remove SNS icons from footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,3 @@
-import imgInstagram from '../img/instagram.png';
-import imgFacebook from '../img/facebook.png';
 import imgTwitter from '../img/twitter.png';
 import imgLinkedIn from '../img/linkedin.png';
 import imgGithub from '../img/github.png';
@@ -11,20 +9,14 @@ const Footer = () => {
         <hr style={{ backgroundColor: '#1ce492' }} />
         <div className="d-flex justify-content-between">
           <div>
-            <a href="https://instagram.com/hakjoon_sim" target="_blank" rel="noopener noreferrer">
-              <img className="sns" src={imgInstagram} alt="instagram" />
-            </a>
-            <a className="ml-1" href="https://facebook.com/hakjoon.sim" target="_blank" rel="noopener noreferrer">
-              <img className="sns" src={imgFacebook} alt="facebook" />
-            </a>
-            <a className="ml-1" href="https://twitter.com/trainto" target="_blank" rel="noopener noreferrer">
-              <img className="sns" src={imgTwitter} alt="twitter" />
-            </a>
             <a className="ml-1" href="https://linkedin.com/in/hakjoon-sim/" target="_blank" rel="noopener noreferrer">
               <img className="sns" src={imgLinkedIn} alt="linkedin" />
             </a>
             <a className="ml-1" href="https://github.com/trainto" target="_blank" rel="noopener noreferrer">
               <img className="sns" src={imgGithub} alt="github" />
+            </a>
+            <a className="ml-1" href="https://twitter.com/trainto" target="_blank" rel="noopener noreferrer">
+              <img className="sns" src={imgTwitter} alt="twitter" />
             </a>
           </div>
           <div>
